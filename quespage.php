3e3684909456c6c43eb171982ffe9123de2b4fbe
ryanpatrick8055 <75079210+ryanpatrick8055@users.noreply.github.com>
2020-11-28 16:11:34 +0530

    Add files via upload

---
+++ quespage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <script type="text/javascript" src="quespage.js"></script>
+    <title>Questions Page</title>
+  </head>
+  
+
+<body>
+
+<li>
+<div>
+<p class="questionc" id="Q1"></p>
+<div>
+  <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+  <label for="question-1-answers-A" id="Q1A"></label>
+</div>
+<div>
+  <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+  <label for="question-1-answers-B" id="Q1B"></label>
+</div>
+<div>
+  <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+  <label for="question-1-answers-C" id ="Q1C"></label>
+</div>
+<div>
+  <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+  <label for="question-1-answers-D" id="Q1D"></label>
+</div>
+</div>
+</li>
+
+
+
+  </body>
+</html>
---
+++ questions.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 28, 2020 at 11:13 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `usersquiz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL,
+  `question` varchar(420) NOT NULL,
+  `option1_correct` varchar(120) NOT NULL,
+  `option2` varchar(120) NOT NULL,
+  `option3` varchar(120) NOT NULL,
+  `option4` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
